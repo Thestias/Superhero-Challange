11307b3abd89bac6f28ec49e8c9de3035e8d9a9e
Thestias <franceschiconrado@gmail.com>
2021-06-27 02:00:35 -0300

    Conditional rendering of NavLinks

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,29 @@
 import { Nav, Navbar, Container } from "react-bootstrap"
+import { useAuth } from "../../contexts/auth-context";
+
 
 function NavigationBar() {
+
+    const { isLogged, setIsLogged } = useAuth()
+
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand>Welcome!</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#">Login</Nav.Link>
-                        <Nav.Link href="#">Lorem</Nav.Link>
-                    </Nav>
+                    {isLogged ?
+                        /* If the user is logged */
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#">Home</Nav.Link>
+                            <Nav.Link href="#">Logout</Nav.Link>
+                        </Nav>
+                        :
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#">Login</Nav.Link>
+                            <Nav.Link href="#">Lorem</Nav.Link>
+                        </Nav>
+                    }
                 </Navbar.Collapse>
             </Container>
         </Navbar>
