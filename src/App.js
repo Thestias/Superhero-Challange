742c1f8577734104e23e39251de9930463798670
Thestias <franceschiconrado@gmail.com>
2021-06-27 10:59:28 -0300

    Fix not exact path / Add Search path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import LoginForm from './components/Login/Login';
 import NavBar from './components/Navbar/Navbar';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+import SearchPage from './components/SearchPage/SearchPage';
 import { Container } from 'react-bootstrap';
 import { useAuth } from './contexts/auth-context'
 
 
 function App() {
-
+  // TODO: i think the redirects are missing the history handling, add it!
+  // TODO: Remember to handle your scret!
   const { isLogged, setIsLogged } = useAuth()
 
   return (
@@ -18,12 +20,16 @@ function App() {
         <NavBar />
         <Container>
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               {isLogged ? <Home /> : <Redirect to="/login" />}
             </Route>
 
-            <Route path="/login">
+            <Route exact path="/login">
               {isLogged ? <Redirect to="/" /> : <LoginForm />}
+            </Route>
+
+            <Route exact path="/search">
+              <SearchPage />
             </Route>
 
           </Switch>
@@ -36,7 +42,7 @@ function App() {
         <p>Credits</p>&nbsp;&nbsp;
         <p>About</p>
       </footer> */}
-    </div>
+    </div >
   );
 }
 export default App;
