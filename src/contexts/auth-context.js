2d11a42831d0570fb6b3dd914217e96950e04792
Thestias <franceschiconrado@gmail.com>
2021-06-27 02:01:52 -0300

    Fix not checking already loged

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -4,7 +4,14 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext()
 
 function AuthProvider(props) {
-    const [isLogged, setIsLogged] = useState(false)
+
+    let logged;
+
+    /* Checks if the user is already logged */
+    if (window.localStorage.getItem('token')) { logged = true }
+    else { logged = false }
+
+    const [isLogged, setIsLogged] = useState(logged)
 
     return (
         <AuthContext.Provider value={{ isLogged, setIsLogged }} {...props} />
